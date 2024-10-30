58ee5f2d4e7f25f33b0a481656eb9bed4fd4eb14
Rifki2002 <rifkiyohandy2002@gmail.com>
2024-10-31 00:17:26 +0700

    Pengumpulan Tugas 3

---
+++ Tugas-3A-Looping/looping.dart/lib/looping.dart
@@ -0,0 +1,48 @@
+// Soal Nomor 1
+
+// void main(List<String> args) {
+//   int i = 2;
+
+//   print('LOOPING PERTAMA');
+//   while (i <= 20) {
+//     print('$i - I love coding');
+//     i += 2;
+//   }
+
+//   i = 20;
+//   print('LOOPING KEDUA');
+//   while (i >= 2) {
+//     print('$i - I will become a mobile developer');
+//     i -= 2;
+//   }
+// }
+
+// Soal Nomor 2
+
+// void main(List<String> args) {
+//   for (int i = 1; i <= 20; i++) {
+//     if (i % 2 != 0 && i % 3 == 0) {
+//       print('$i - I Love Coding');
+//     } else if (i % 2 == 0) {
+//       print('$i - Berkualitas');
+//     } else {
+//       print('$i - Santai');
+//     }
+//   }
+// }
+
+// Soal Nomor 3
+
+// void main(List<String> args) {
+//   for (int i = 1; i <= 4; i++) {
+//     print('#' * 8);
+//   }
+// }
+
+// Soal Nomor 4
+
+// void main(List<String> args) {
+//   for (int i = 1; i <= 7; i++) {
+//     print('#' * i);
+//   }
+// }
---
+++ Tugas-3B-Function/tugasfunction.dart/lib/tugasfunction.dart
@@ -0,0 +1,59 @@
+// Soal Nomor 1
+
+// String teriak() {
+//   return "Halo Sanbers!";
+// }
+
+// void main(List<String> args) {
+//   print(teriak());
+// }
+
+// Soal Nomor 2
+
+// int kalikan(int a, int b) {
+//   return a * b;
+// }
+
+// void main(List<String> args) {
+//   var num1 = 12;
+//   var num2 = 4;
+
+//   var hasilKali = kalikan(num1, num2);
+//   print("Hasil Perkalian Adalah : $hasilKali");
+// }
+
+// Soal Nomor 3
+
+// String introduce(String name, int age, String address, String hobby) {
+//   return 'Nama saya $name, umur saya $age, Alamat saya di $address, dan saya punya hobby yaitu $hobby';
+// }
+
+// void main(List<String> args) {
+//   var name = "Agus";
+//   var age = 30;
+//   var address = "Jln. Malioboro, Yogyakarta";
+//   var hobby = "Gaming!";
+//   var perkenalan = introduce(name, age, address, hobby);
+//   print(perkenalan);
+// }
+
+// Soal Nomor 4
+
+
+// int faktorial(int n) {
+//   if (n <= 0) {
+//     return 1;
+//   }
+//   int hasil = 1;
+//   for (int i = n; i > 0; i--) {
+//     hasil *= i;
+//   }
+
+//   return hasil;
+// }
+
+// void main() {
+//   var angka = 6;
+//   var hasilFaktorial = faktorial(angka);
+//   print("Faktorial dari $angka! adalah: $hasilFaktorial");
+// }
